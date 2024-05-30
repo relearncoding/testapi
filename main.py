af24bbbb3fa1958aa99dafe0630ca5ff36906df3
LR <lakshmi.remani@camunda.com>
2024-05-30 06:36:30 +0100

    addingresponse

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello World"}
+    return "Hello World"
 
 @app.get("/decision")
 def read_root():
-    return {'1'}
+    return 1
 
 @app.get("/number")
 def read_root():
-    return {'90'}
+    return 90
